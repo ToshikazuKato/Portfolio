487cbcbddc805eeb3260ee4497ff9aea8c9869e4
ToshikazuKato <kazu1427bsk@gmail.com>
2019-09-21 03:24:15 +0900

    photo bug fixed

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ class About extends Component {
 	render() {
 		return (
 			<div id="about">
-				<img src="/images/Me.JPG" alt="myself"></img>
+				<img src="/images/Me.jpg" alt="myself"></img>
 				<h3>Hi, I'm Toshikazu Horioka</h3>
 			</div>
 		);
