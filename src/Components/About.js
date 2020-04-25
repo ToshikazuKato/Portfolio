2962a671f777f7741674a78b7f99f906b82132ed
ToshikazuKato <kazu1427bsk@gmail.com>
2020-04-26 02:42:40 +0900

    test image

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,8 +4,11 @@ class About extends Component {
 
 	render() {
 		return (
-			<div id="about">
-				<img src="/images/Me.jpg" alt="myself"></img>
+			<div id='about'>
+				<img
+					src='/images/me-x-small.jpg'
+					alt='myself'
+				></img>
 				<h3>Hi, I'm Toshikazu Horioka</h3>
 			</div>
 		);
