95b6d88e00b5fbb4f4ca0342715b9f50bccdafb3
ToshikazuKato <kazu1427bsk@gmail.com>
2020-04-26 01:50:26 +0900

    update

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -10,7 +10,7 @@ class Intro extends Component {
 				<div>
 					<h2>Toshikazu Horioka</h2>
 					<p>I'm a Japanese Fullstack Engineer based in Tokyo.</p>
-					<p>I build web services that make the world better</p>
+					<p> I build web services that make the world better</p>
 					<p> and help people live better.</p>
 				</div>
 			</div>
